6435ff678d04a301578955f383b6e6eb8f9d6db4
Zy <3935823763@qq.com>
2026-08-09 13:53:21 +0800

    Add config files and entry point

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { I18nProvider } from '@/lib/i18n';
+import { AuthProvider } from '@/lib/auth';
+import { ThemeProvider } from '@/lib/theme';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ThemeProvider>
+      <I18nProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </I18nProvider>
+    </ThemeProvider>
+  </StrictMode>
+);
